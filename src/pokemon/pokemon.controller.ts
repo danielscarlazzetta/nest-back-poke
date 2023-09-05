7856fac52e4cc2dab5e16fd270231a4656284875
danielscarlazzetta <daniel.scarlazzetta@gmail.com>
2023-09-04 23:53:30 -0300

    creacion de findOne

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
@@ -8,6 +8,8 @@ export class PokemonController {
   constructor(private readonly pokemonService: PokemonService) {}
 
   @Post()
+  @HttpCode(200)
+  //@HttpCode( HttpStatus.OK)
   create(@Body() createPokemonDto: CreatePokemonDto) {
     return this.pokemonService.create(createPokemonDto);
   }
@@ -17,9 +19,9 @@ export class PokemonController {
     return this.pokemonService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.pokemonService.findOne(+id);
+  @Get(':term')
+  findOne(@Param('term') term: string) {
+    return this.pokemonService.findOne(term);
   }
 
   @Patch(':id')
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -13,17 +13,44 @@ export class PokemonService {
      private readonly _pokemonModel: Model<Pokemon>){
   }
 
-  create(createPokemonDto: CreatePokemonDto) {
-    createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    return createPokemonDto;
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase().trim();
+    try {
+      const pokemon = await this._pokemonModel.create( createPokemonDto )
+      return pokemon;
+      
+    } catch (error) {
+      if(error.code === 11000){
+        throw new BadRequestException(`Ya existe el pokemon en la Base de Datos! ${JSON.stringify(error.keyValue) }`)
+      }
+      console.log(error)
+        throw new InternalServerErrorException(`No se puede crear pokemon`);
+    }
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+
+    let pokemon : Pokemon;
+
+    if(!isNaN(+term)){ 
+      pokemon = await this._pokemonModel.findOne({no: term})
+    }
+
+    if(!pokemon && isValidObjectId(term)){
+      pokemon = await this._pokemonModel.findById(term); 
+    }
+    
+    if(!pokemon) { 
+      pokemon = await this._pokemonModel.findOne({name: term.toLowerCase().trim()})
+    }
+    
+    if(!pokemon) throw new NotFoundException(`No existe ${term}`);
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
